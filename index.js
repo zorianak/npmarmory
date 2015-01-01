1f127b05ed18de29cd76d36aec29c60e4e6d01bd
Kim Holmes <yes@kimiscool.me>
2014-12-31 20:41:15 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    cat: function(html) {
+        return 'meow';
+    }
+}
---
+++ test/index.js
@@ -0,0 +1,11 @@
+var should = require('chai').should(),
+    npmarmory = require('../index'),
+    cat = npmarmory.cat;
+
+describe('#cat', function(){
+    it('should return some text', function() {
+        var foobar = {
+            cat.should.equal('meow');
+        }
+    })
+}
