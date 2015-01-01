1c9ac40192cb644d5f47e1aebc870acf8a94ec6c
Kim Holmes <yes@kimiscool.me>
2015-01-01 14:34:33 -0800

    Adding grunt for future, and added test scripts

---
+++ test/index_test.js
@@ -0,0 +1,51 @@
+var chai = require('chai'),
+    should = require('chai').should(),
+    assert = chai.assert,
+    expect = chai.expect,
+    npmarmory = require('../index'),
+    escape = npmarmory.escape,
+    unescape = npmarmory.unescape;
+
+describe('#escape', function() {
+  it('converts & into &amp;', function() {
+    escape('&').should.equal('&amp;');
+  });
+
+  it('converts " into &quot;', function() {
+    escape('"').should.equal('&quot;');
+  });
+
+  it('converts \' into &#39;', function() {
+    escape('\'').should.equal('&#39;');
+  });
+
+  it('converts < into &lt;', function() {
+    escape('<').should.equal('&lt;');
+  });
+
+  it('converts > into &gt;', function() {
+    escape('>').should.equal('&gt;');
+  });
+});
+
+describe('#unescape', function() {
+  it('converts &amp; into &', function() {
+    unescape('&amp;').should.equal('&');
+  });
+
+  it('converts &quot; into "', function() {
+    unescape('&quot;').should.equal('"');
+  });
+
+  it('converts &#39; into \'', function() {
+    unescape('&#39;').should.equal('\'');
+  });
+
+  it('converts &lt; into <', function() {
+    unescape('&lt;').should.equal('<');
+  });
+
+  it('converts &gt; into >', function() {
+    unescape('&gt;').should.equal('>');
+  });
+});
