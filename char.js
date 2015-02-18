f9df609ea451541013a9b6161d6cf5626eacc013
Kim Holmes <yes@kimiscool.me>
2015-02-17 17:37:46 -0800

    add rest of races

--- char.js
+++ char.js
@@ -13,7 +13,7 @@ module.exports = {
             console.log('Invalid character');
         } else {
             // we have info, let's summon this char
-            // http://us.battle.net/api/wow/character/Windrunner/Caligraphy?fields=stats
+            // http://us.battle.net/api/wow/character/Windrunner/Caligraphy?fields=stats,items&locale=en_US&jsonp=callback&apikey=fbgams9zxkqsezwqaavxxk9u8rkvxxkn
             var url = 'https://' + region + '.battle.net' +'/api/wow/character/' + realm + '/' + name;
             var statsUrl = url + '?fields=stats,items&locale=en_US&jsonp=callback&apikey=fbgams9zxkqsezwqaavxxk9u8rkvxxkn';
 
@@ -93,6 +93,18 @@ module.exports = {
             this.race = 'worgen';
         }else if (raceNumber == 25){
             this.race = 'pandaren';
+        }else if (raceNumber == 10){
+            this.race = 'blood elf';
+        }else if (raceNumber == 5){
+            this.race = 'undead';
+        }else if (raceNumber == 4){
+            this.race = 'goblin';
+        }else if (raceNumber == 6){
+            this.race = 'tauren';
+        }else if (raceNumber == 8){
+            this.race = 'troll';
+        }else if (raceNumber == 2){
+            this.race = 'orc';
         }
 
         console.log(this.race);
