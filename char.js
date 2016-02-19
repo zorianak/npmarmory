b3f94a771278526ca560793c099f6d6b4cd4d83c
Kim Holmes <yes@kimiscool.me>
2016-02-19 14:45:37 -0800

    Add request. No longer using jQ/jsdom. Now parses new API setup properly

--- char.js
+++ char.js
@@ -1,7 +1,8 @@
 // Generally don't want to use jQ for NPM modules. However,
 // the tests complain and better to just copy another person's
 // window than to recreat it.
-var $ = require('jquery')(require("jsdom").jsdom().parentWindow);
+var $ = require('jquery')(require("jsdom").jsdom().parentWindow),
+	request = require('request');
 
 // This is an object to create a character object
 // We pass in the data from the original importChar
@@ -22,36 +23,27 @@ module.exports = {
             // we have info, let's summon this char
             // https://us.api.battle.net/wow/character/hyjal/Calligraphy?fields=stats,talents,items&locale=en_US&jsonp=true&apikey=fbgams9zxkqsezwqaavxxk9u8rkvxxkn
             var url = 'https://' + region + '.api.battle.net/wow/character/' + realm + '/' + name;
-            var statsUrl = url + '?fields=stats,items,talents&locale=en_US&jsonp=true&apikey=fbgams9zxkqsezwqaavxxk9u8rkvxxkn';
+            var statsUrl = url + '?fields=stats,items,talents&locale=en_US&jsonp=data&apikey=fbgams9zxkqsezwqaavxxk9u8rkvxxkn';
 			console.log(url);
 //			console.log(statsUrl);
 //            var statsUrl = 'http://us.battle.net/api/wow/character/Stormreaver/Calligraphy?fields=stats,items,talents&locale=en_US&jsonp=callback&apikey=fbgams9zxkqsezwqaavxxk9u8rkvxxkn';
-            // So, if we wanted to really impress someone, we could do this
-            // in vanilla. Make an httprequest variable, set that, do a ton
-            // of things. However, we're already using jQ for some of our
-            // tests and console returns... and frankly, jQ's ajax
-            // statements are clean enough we MIGHT AS WELL use it here.
-            // Testing both ways, did not see a performance gain either way,
-            // and this will be *far* more maintainable
-            $.ajax({
-                url: statsUrl,
-                dataType: 'JSONP',
-                jsonpCallback: 'callback',
-                type: 'GET',
-                success: function (data) {
-                    console.log('Done fetching!');
-                }
-            }).done(function(data) {
-				
-            	console.log('Done!');
-                // when it's done, we want to call something to actually make the char I guess
-                fn(data);
-            });
+            // swapping to using request. built-in NPM stuff requires me to update node.
+			request(statsUrl, function(error, response, body){
+				if(!error && response.statusCode == 200) {
+					var f = new Function("data", body);
+					
+					// ty jsonp for being a poop
+					f(function(json) {
+						fn(json);
+					});
+				}
+			})
 
         }
     },
     // Character object used to creat characters
     Char: function(data) {
+		
         var stats = data["stats"] || {},
             items = data["items"] || {};
         this.name = data["name"] || "Anon monk";
@@ -75,6 +67,7 @@ module.exports = {
         this.Mdps = stats["mainHandDmgMax"] || 0;
         this.mdps = stats["mainHandDmgMin"] || 0;
         this.mhs = stats["mainHandSpeed"] || 0;
+		this.mainDpsAvgd = stats["mainHandDps"] || 0;
         
         this.ohdps = stats["offHandDmgMin"] || 0;
         this.Ohdps = stats["offHandDmgMax"] || 0;
@@ -117,26 +110,28 @@ module.exports = {
         // TODO: Add horde
 
         // set talents
-        if(data["talents"][1]["selected"] == true) {
-           var talent = data["talents"][1]["talents"] || "";
-        } else {
-           var talent = data["talents"][0] || "";
-        }
-//        var talent = data["talents"][1] || "";
-        this.talent15 = talent[2]["column"] || "";
-        this.talent15name = talent[2]["spell"]["name"] || "No Talent";
-        this.talent30 = talent[1]["column"] || "";
-        this.talent30name = talent[1]["spell"]["name"] || "No Talent";
-        this.talent45 = talent[5]["column"] || "";
-        this.talent45name = talent[5]["spell"]["name"] || "No Talent";
-        this.talent60 = talent[0]["column"] || "";
-        this.talent60name = talent[0]["spell"]["name"] || "No Talent";
-        this.talent75 = talent[3]["column"] || "";
-        this.talent75name = talent[3]["spell"]["name"] || "No Talent";
-        this.talent90 = talent[4]["column"] || "";
-        this.talent90name = talent[4]["spell"]["name"] || "No Talent";
-        this.talent100 = talent[6]["column"] || "";
-        this.talent100name = talent[6]["spell"]["name"] || "No Talent";
+		   if(data["talents"][1]["selected"] == true) {
+			   var talent = data["talents"][1]["talents"] || "";
+			} else {
+			   var talent = data["talents"][0] || "";
+			}
+	//        var talent = data["talents"][1] || "";
+			this.talent15 = talent[2]["column"] || "";
+			this.talent15name = talent[2]["spell"]["name"] || "No Talent";
+			this.talent30 = talent[1]["column"] || "";
+			this.talent30name = talent[1]["spell"]["name"] || "No Talent";
+			this.talent45 = talent[5]["column"] || "";
+			this.talent45name = talent[5]["spell"]["name"] || "No Talent";
+			this.talent60 = talent[0]["column"] || "";
+			this.talent60name = talent[0]["spell"]["name"] || "No Talent";
+			this.talent75 = talent[3]["column"] || "";
+			this.talent75name = talent[3]["spell"]["name"] || "No Talent";
+			this.talent90 = talent[4]["column"] || "";
+			this.talent90name = talent[4]["spell"]["name"] || "No Talent";
+			this.talent100 = talent[6]["column"] || "";
+			this.talent100name = talent[6]["spell"]["name"] || "No Talent";
+		
+        
 
 //        console.log(talent);
         this.tacoCat = 'Tacocat is a palindrome';
